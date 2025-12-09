77852b9f61cba40c6a37e119fbbc9fb5a70b6a38
Bernardo António Borda d'Água <berna.agua@protonmail.com>
2025-12-09 03:33:01 +0000

    add redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,9 @@
 import { Box, Button, HStack, VStack } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <>
-      <Box style={{ height: '100vh' }}>
-        <VStack style={{ height: '80%' }}>
-          <Button style={{ width: '90%', height: '70px', backgroundColor: 'green' }}>
-            Goal
-          </Button>
-          <Button style={{ width: '90%', height: '70px', backgroundColor: 'red' }}>
-            Save
-          </Button>
-          <Button style={{ width: '90%', height: '70px', backgroundColor: 'blue' }}
-            asChild>
-            <Link href="/miss/choose_player">
-              Miss
-            </Link>
-          </Button>
-          <Button style={{ width: '90%', height: '70px', backgroundColor: 'orange' }}>
-            Block
-          </Button>
-          {/* <Button style={{ width: '90%', height: '70px', backgroundColor: 'yellow' }}>
-            Technical Foul
-          </Button> */}
-          <Button style={{ width: '90%', height: '70px', backgroundColor: 'purple' }}>
-            Turnover
-          </Button>
-          {/* <Button style={{ width: '90%', height: '70px', backgroundColor: 'gray' }}>
-            Sanction
-          </Button> */}
-        </VStack>
-        <HStack style={{ height: '20%', justifyContent: 'space-between' }}>
-          <Button style={{ width: '45%', height: '70px', backgroundColor: 'black', color: 'white' }}>
-            Sanction
-          </Button>
-          <Button style={{ width: '45%', height: '70px', backgroundColor: 'brown', color: 'white' }}>
-            Technical Foul
-          </Button>
-        </HStack>
-      </Box>
-    </>
-  );
+export default async function Home() {
+  redirect(`/main`);
 }
+
