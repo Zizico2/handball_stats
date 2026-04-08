801dfbe4087610bf77990abd0a8df52b12c3a0d1
Bernardo António Borda d'Água <berna.agua@protonmail.com>
2026-04-08 21:46:07 +0100

    normalize migrations

--- drizzle/20260408204304_cool_molten_man/migration.sql
+++ drizzle/20260408204304_cool_molten_man/migration.sql
@@ -28,7 +28,9 @@ CREATE TABLE `player_events` (
 	`event_group` text NOT NULL,
 	`shot_goal` integer,
 	`shot_direction` text,
-	CONSTRAINT `fk_player_events_user_id_game_local_id_games_user_id_local_id_fk` FOREIGN KEY (`user_id`,`game_local_id`) REFERENCES `games`(`user_id`,`local_id`)
+	`shot_aim` text,
+	CONSTRAINT `fk_player_events_user_id_game_local_id_games_user_id_local_id_fk` FOREIGN KEY (`user_id`,`game_local_id`) REFERENCES `games`(`user_id`,`local_id`),
+	CONSTRAINT "shot_direction_required_for_shot" CHECK(`event_type` != 'shot' OR `shot_direction` IS NOT NULL)
 );
 --> statement-breakpoint
 CREATE TABLE `team_players` (
